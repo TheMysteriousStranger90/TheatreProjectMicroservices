25a7b9706320fbe97a8e3ca351f513e17905ece6
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2025-01-10 17:51:12 +0200

    shopping_cart part - 2

---
+++ TheatreProject.ShoppingCartAPI/Validators/CartDetailsValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using TheatreProject.ShoppingCartAPI.Models.DTOs;
+
+namespace TheatreProject.ShoppingCartAPI.Validators;
+
+public class CartDetailsValidator : AbstractValidator<CartDetailsDto>
+{
+    public CartDetailsValidator()
+    {
+        RuleFor(x => x.PerformanceId).NotEmpty();
+        RuleFor(x => x.SeatNumbers).NotEmpty()
+            .Matches(@"^[A-Z][0-9]+(,[A-Z][0-9]+)*$")
+            .WithMessage("Seat numbers must be in format: A1,B2,C3");
+        RuleFor(x => x.Quantity).GreaterThan(0);
+        RuleFor(x => x.PricePerTicket).GreaterThan(0);
+        RuleFor(x => x.SubTotal).GreaterThan(0);
+    }
+}
---
+++ TheatreProject.ShoppingCartAPI/Validators/CartDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using TheatreProject.ShoppingCartAPI.Models.DTOs;
+
+namespace TheatreProject.ShoppingCartAPI.Validators;
+
+public class CartDtoValidator : AbstractValidator<CartDto>
+{
+    public CartDtoValidator()
+    {
+        RuleFor(x => x.CartHeader).NotNull();
+        RuleFor(x => x.CartDetails).NotNull().NotEmpty();
+        RuleForEach(x => x.CartDetails).SetValidator(new CartDetailsValidator());
+    }
+}
