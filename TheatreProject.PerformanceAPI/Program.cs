3daef9e262ddfa2c74ada772245ad3bf26f00fa5
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2025-01-20 08:54:25 +0200

    Added fixes to PerformanceAPI

--- TheatreProject.PerformanceAPI/Program.cs
+++ TheatreProject.PerformanceAPI/Program.cs
@@ -1,32 +1,12 @@
-using FluentValidation.AspNetCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 using TheatreProject.PerformanceAPI.Data;
 using TheatreProject.PerformanceAPI.Extensions;
-using TheatreProject.PerformanceAPI.Mapping;
-using TheatreProject.PerformanceAPI.Repositories;
-using TheatreProject.PerformanceAPI.Services;
-using TheatreProject.PerformanceAPI.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
-
-builder.Services.AddFluentValidation(fv => { fv.RegisterValidatorsFromAssemblyContaining<PerformanceDtoValidator>(); });
-
-builder.Services.AddMemoryCache();
-builder.Services.AddSingleton<ICacheKeyService, CacheKeyService>();
-
-builder.Services.AddScoped<IPerformanceRepository, PerformanceRepository>();
-
-builder.Services.AddScoped<ValidationFilter>();
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddJwtBearerServices();
 
 var app = builder.Build();
@@ -42,7 +22,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-
+    
     app.MapGet("/", context =>
     {
         context.Response.Redirect("/swagger/index.html");
